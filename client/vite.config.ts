377cc18a9d56aac6e9b6ded262857920b8ab3e44
molatlhegiM <139362696+molatlhegiM@users.noreply.github.com>
2025-08-22 16:05:01 +0200

    Initial commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2Gether-Travels/', // 👈 very important for GitHub Pages
-})
+  base: '/2Gether-Travels/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
