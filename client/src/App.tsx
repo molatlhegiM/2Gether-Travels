407c7c33b105068769acb25b265194bce424a617
molatlhegiM <139362696+molatlhegiM@users.noreply.github.com>
2025-08-22 19:37:52 +0200

    initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,68 @@
-import { Switch, Route } from "wouter";
+import { Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import Packages from "@/pages/packages";
-import Hotels from "@/pages/hotels";
-import Transfers from "@/pages/transfers";
-import Tours from "@/pages/tours";
-import Concierge from "@/pages/concierge";
-import About from "@/pages/about";
-import Partners from "@/pages/partners";
-import Resources from "@/pages/resources";
-import Contact from "@/pages/contact";
-import Booking from "@/pages/booking";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import Home from "@/pages/Home";
+import Packages from "@/pages/Packages";
+import Hotels from "@/pages/Hotels";
+import Transfers from "@/pages/Transfers";
+import Tours from "@/pages/Tours";
+import Concierge from "@/pages/Concierge";
+import About from "@/pages/About";
+import Partners from "@/pages/Partners";
+import Resources from "@/pages/Resources";
+import Contact from "@/pages/Contact";
+import Booking from "@/pages/Booking";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 function Router() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/packages" component={Packages} />
-          <Route path="/hotels" component={Hotels} />
-          <Route path="/transfers" component={Transfers} />
-          <Route path="/tours" component={Tours} />
-          <Route path="/concierge" component={Concierge} />
-          <Route path="/about" component={About} />
-          <Route path="/partners" component={Partners} />
-          <Route path="/resources" component={Resources} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/booking" component={Booking} />
-          <Route component={NotFound} />
-        </Switch>
+        {/* Routes */}
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/packages">
+          <Packages />
+        </Route>
+        <Route path="/hotels">
+          <Hotels />
+        </Route>
+        <Route path="/transfers">
+          <Transfers />
+        </Route>
+        <Route path="/tours">
+          <Tours />
+        </Route>
+        <Route path="/concierge">
+          <Concierge />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/partners">
+          <Partners />
+        </Route>
+        <Route path="/resources">
+          <Resources />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/booking">
+          <Booking />
+        </Route>
+
+        {/* Catch-all route (NotFound) */}
+        <Route>
+          <NotFound />
+        </Route>
       </main>
       <Footer />
     </div>
